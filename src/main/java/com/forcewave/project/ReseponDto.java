869605881b0bad68be297b0ba8e0222571a03ab3
cotiful <ysh7091@gmail.com>
2022-06-30 10:06:49 +0900

    jpa nativequery 오류 수정중

---
+++ src/main/java/com/forcewave/project/ReseponDto.java
@@ -0,0 +1,13 @@
+package com.forcewave.project;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReseponDto {
+
+    private String emdname;
+}
